70873dcdf638bca349333c37f532a0db9475d342
camicarrizouade <camicarrizo@uade.edu.ar>
2025-09-30 11:53:00 -0300

    Se modifico el nombre de modificar prestamos a actualizar prestamos

--- Biblioteca/prestamos/__init__.py
+++ Biblioteca/prestamos/__init__.py
@@ -1,6 +1,6 @@
 from .crear_prestamos import crear_prestamo
 from .prestamos import listar_prestamos, buscar_prestamo_por_id
-from .modificar_prestamo import actualizar_prestamo
+from .actualizar_prestamo import actualizar_prestamo
 from .baja_prestamo import bajar_prestamo
 
 __all__ = [
