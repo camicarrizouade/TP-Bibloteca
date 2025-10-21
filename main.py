718cc22ca431d51657919e6fca370dfaecb43811
camicarrizouade <camicarrizo@uade.edu.ar>
2025-10-21 15:44:30 -0300

    Agregue funciones para la gestion de prestamos y socios

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ from Biblioteca.consultas import mostrar_tabla
 from Biblioteca.validaciones import normalizar_texto, entero_positivo, validar_dni, texto_no_vacio
 
 #Funciones lambda
+#libros
 creacion_de_libro= crear_libro(normalizar_texto(input("Titulo:"), normalizar_texto(input("Autor:")), normalizar_texto(input("Género:")), entero_positivo(input("Anio:"),entero_positivo("Cantidad de libros a ingresar:"))))
 mostrar_libros= mostrar_tabla(C.ENCABEZADOS_LIBROS,ST.M_LIBROS)
 actualizacion_libro= actualizar_libro(input("Ingrese el id del libro a modificar:"))
 dar_de_baja_libros=bajar_libro(input("Ingrese el id del libro a dar de baja:"))
-
-
+#socios
+creacion_de_socio=crear_socio(normalizar_texto(input("Nombre_")),validar_dni(input("DNI:")))
+mostrar_socios=mostrar_tabla(C.ENCABEZADOS_SOCIOS,ST.M_SOCIOS)
+actualizacion_socio=actualizar_socio(input("Ingrese el id del socio a modificar:"))
+dar_de_baja_socios=bajar_socio(input("Ingrese el id del socio a dar de baja:"))
+#prestamos
+creacion_de_prestamo=crear_prestamo(input("Socio ID:"), input("Libro ID:"))
+mostrar_prestamos=mostrar_tabla(C.ENCABEZADOS_PRESTAMOS,ST.M_PRESTAMOS)
+actualizacion_prestamo=actualizacion_prestamo(input("ID de préstamo a actualizar:"))
+dar_de_baja_prestamo=bajar_prestamo(input("ID de prestamo a dar de baja:"))
 #Funciones mas complejas en si
 def mostrar_menu(titulo, opciones_dict):
     """Imprime el menu de forma legible para el usuario."""
@@ -61,8 +70,8 @@ def gestion_socios():
         opciones={
             "1": ("Crear socio", creacion_de_socio ),
             "2": ("Mostrar socios", mostrar_socios ),
-            "3": ("Actualizar socio", actualizar_socio ),
-            "4": ("Dar de baja socios", bajar_socio ),
+            "3": ("Actualizar socio", actualizacion_socio),
+            "4": ("Dar de baja socios", dar_de_baja_socios),
             "5": ("Volver al menú anterior.", "VOLVER."), 
         }
 
@@ -88,9 +97,9 @@ def gestion_prestamos():
     """Sub menu de gestion de prestamos con excepciones para evitar usar muchos if anidados"""
     while True:
         opciones={
-            "1": ("Crear prestamo", creacion_prestamos ),
+            "1": ("Crear prestamo", creacion_de_prestamo ),
             "2": ("Mostrar prestamos", mostrar_prestamos ),
-            "3": ("Actualizar prestamo", actualizar_prestamo),
+            "3": ("Actualizar prestamo", actualizacion_prestamo),
             "4": ("Dar de baja prestamos", dar_de_baja_prestamo),
             "5": ("Volver al menú anterior.", "VOLVER."), 
         }
@@ -126,12 +135,14 @@ def menu_principal(gestion_libros, gestion_prestamos, gestion_socios):
 
         try:
 
-            accion_a_ejecutar= menu_opciones[opcion]
+            tupla_completa= menu_opciones[opcion]
+
+            accion_a_ejecutar=tupla_completa[1]
 
             if accion == "SALIR":
                 print("Finalizando el sistema...")
                 break
-                
+            
             accion_a_ejecutar()
         
         except KeyError:
